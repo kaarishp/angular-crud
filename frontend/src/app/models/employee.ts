e89a63fd35ea9659232bd886c2c3ad25bd0bfdec
kaarishp <kingkaarish@gmail.com>
2024-04-04 11:43:28 -0400

    added employee model

---
+++ frontend/src/app/models/employee.ts
@@ -0,0 +1,25 @@
+export class Employee {
+    id: string; 
+    firstname: string; 
+    lastname: string; 
+    email: string; 
+    gender: string; 
+    salary: number; 
+
+    constructor(
+      id: string,
+      firstname: string,
+      lastname: string,
+      email: string,
+      gender: string,
+      salary: number
+    ) {
+      this.id = id;
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.email = email;
+      this.gender = gender;
+      this.salary = salary;
+    }
+  }
+  

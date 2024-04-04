0ddae5475772cf0523907d257b024246d6c63f38
kaarishp <kingkaarish@gmail.com>
2024-04-04 11:41:49 -0400

    added employee service

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "signup", component: SignupComponent }, // Route to SignupComponent
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+
+export class AppRoutingModule {}

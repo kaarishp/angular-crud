22ee4ca190e5c34764528c976bce20a17685c9a0
kaarishp <kingkaarish@gmail.com>
2024-04-05 14:43:12 -0400

    updated backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -25,12 +25,5 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
-
 const UserModel = mongoose.model("User", UserSchema);
 module.exports = UserModel;
